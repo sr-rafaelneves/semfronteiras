c9337b5bf1c334e9491c8de1445e686fa428e95e
Rafael Augusto <sr.rafaelaugusto@gmail.com>
2024-04-19 14:29:22 -0300

    :tada: Commit Inicial

---
+++ src/main/java/br/com/rnconsulting/semfronteiras/SemfronteirasApplication.java
@@ -0,0 +1,13 @@
+package br.com.rnconsulting.semfronteiras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemfronteirasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemfronteirasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rnconsulting/semfronteiras/SemfronteirasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rnconsulting.semfronteiras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemfronteirasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
