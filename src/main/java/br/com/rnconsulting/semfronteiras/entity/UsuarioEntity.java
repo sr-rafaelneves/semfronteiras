cb850bad9af928f58123614a01ee226e81b2c6de
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-09-18 18:00:10 -0300

    update: Construtor Completo da UsuarioEntity

--- src/main/java/br/com/rnconsulting/semfronteiras/entity/UsuarioEntity.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/entity/UsuarioEntity.java
@@ -1,6 +1,7 @@
 package br.com.rnconsulting.semfronteiras.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
