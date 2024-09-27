0c24ee1d5225ce0bd00dd685fc4a1ab120fa0b17
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-09-27 18:13:02 -0300

    Update AuthorizationController.java

--- src/main/java/br/com/rnconsulting/semfronteiras/security/controller/AuthorizationController.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/security/controller/AuthorizationController.java
@@ -1,10 +1,16 @@
 package br.com.rnconsulting.semfronteiras.security.controller;
 
+import br.com.rnconsulting.semfronteiras.entity.UsuarioEntity;
 import br.com.rnconsulting.semfronteiras.security.entity.TokenResponseRecord;
 import br.com.rnconsulting.semfronteiras.security.entity.User;
 import br.com.rnconsulting.semfronteiras.security.entity.UserRecord;
 import br.com.rnconsulting.semfronteiras.security.repository.UserRepository;
 import br.com.rnconsulting.semfronteiras.security.service.TokenService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("auth")
+@Tag(name = "Autenticação", description = "")
 public class AuthorizationController {
 
     @Autowired
@@ -30,6 +37,10 @@ public class AuthorizationController {
     TokenService tokenService;
 
     @PostMapping("/token")
+    @Operation(summary = "Geração de Token Bearer", description = "")
+    @ApiResponse(responseCode = "200", description = "OK",
+            content = @Content(mediaType = "application/json",
+                    schema = @Schema(implementation =  TokenResponseRecord.class)))
     public ResponseEntity login (@RequestBody @Validated UserRecord userRecord){
 
         // System.out.println(new BCryptPasswordEncoder().encode(userRecord.senha())); // ver a senha criptografada
