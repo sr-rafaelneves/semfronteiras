522abde9d34a7968e8a815eaa8cd6a38c422c7b5
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-09-27 18:12:54 -0300

    Update SemfronteirasApplication.java

--- src/main/java/br/com/rnconsulting/semfronteiras/SemfronteirasApplication.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/SemfronteirasApplication.java
@@ -1,9 +1,12 @@
 package br.com.rnconsulting.semfronteiras;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "SemFronteiras API",version = "1.0", description = "API do sistema Sem Fronteiras"))
 public class SemfronteirasApplication {
 
 	public static void main(String[] args) {
