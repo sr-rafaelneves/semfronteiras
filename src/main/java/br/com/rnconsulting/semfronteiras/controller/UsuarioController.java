98f0039b2a2a84410bfdd6dafe5da75b8c411675
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-11-26 14:21:13 -0300

    Update UsuarioController.java

--- src/main/java/br/com/rnconsulting/semfronteiras/controller/UsuarioController.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/controller/UsuarioController.java
@@ -1,24 +1,17 @@
 package br.com.rnconsulting.semfronteiras.controller;
 
+import br.com.rnconsulting.semfronteiras.dto.usuario.RequestUsuarioCadastrarDTO;
 import br.com.rnconsulting.semfronteiras.entity.UsuarioEntity;
-import br.com.rnconsulting.semfronteiras.repositories.UsuarioRepository;
 import br.com.rnconsulting.semfronteiras.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.websocket.server.PathParam;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.http.HttpClient;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/usuario")
@@ -60,13 +53,25 @@ public class UsuarioController {
         return usuarioService.pesqUsuarioId(email);
     }
 
-    @PostMapping
-    @Operation(summary = "Cadastrar Novo Usuário", description = "")
-    @ApiResponse(responseCode = "201", description = "Sucesso",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation =  UsuarioEntity.class)))
-    public ResponseEntity<?> criarUsuario(@RequestBody UsuarioEntity usuarioEntity) {
+    @PostMapping("/{cpfcnpj}")
+    @Operation(summary = "Cadastrar Novo Usuário", description = """
+            Informar CPFCPNJ da Pessoa na URL (Obrigatório) \n
+            Campo "email" (Obrigatório) \n
+            Campo "Senha" com no minimo 4 caracteres (Obrigatório) \n
+            Campo "situacao" Informar "A" = Ativo | "I" = Inativo | Default = "A" (Opcional) \n
+            """)
+    @ApiResponse(responseCode = "201", description = "Sucesso")
+    public ResponseEntity<?> criarUsuario(@PathVariable String cpfcnpj, @RequestBody RequestUsuarioCadastrarDTO usuarioRequest) {
 
+            UsuarioEntity usuarioEntity = new UsuarioEntity();
+
+            usuarioEntity.setEmail(usuarioRequest.getEmail());
+            usuarioEntity.setSenha(usuarioRequest.getSenha());
+            usuarioEntity.setSituacao(usuarioRequest.getSituacao());
+
+            if(cpfcnpj != null) {
+                usuarioEntity.getPessoaEntity().setCpfcnpj(cpfcnpj);
+            }
         return usuarioService.criarUsuario(usuarioEntity);
     }
 
