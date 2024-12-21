05616fbaca1345af4c262a95194206d99e218714
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-12-20 22:10:16 -0300

    Update UsuarioController.java

--- src/main/java/br/com/rnconsulting/semfronteiras/controller/UsuarioController.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/controller/UsuarioController.java
@@ -1,14 +1,17 @@
 package br.com.rnconsulting.semfronteiras.controller;
 
 import br.com.rnconsulting.semfronteiras.dto.usuario.RequestUsuarioCadastrarDTO;
+import br.com.rnconsulting.semfronteiras.dto.usuario.ResponseUsuarioConsultarDTO;
 import br.com.rnconsulting.semfronteiras.entity.UsuarioEntity;
 import br.com.rnconsulting.semfronteiras.services.UsuarioService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,33 +27,32 @@ public class UsuarioController {
 
     @GetMapping
     @Operation(summary = "Consulta de Todos os Usuários", description = "Realiza uma consulta de todos os usuários existentes.")
-    @ApiResponse(responseCode = "201", description = "Sucesso",
+    @ApiResponse(responseCode = "200", description = "Sucesso",
             content = @Content(mediaType = "application/json",
-                    array = @ArraySchema(schema = @Schema(implementation =  UsuarioEntity.class))))
-    public ResponseEntity<?> listarUsuarios() {
+                    array = @ArraySchema(schema = @Schema(implementation = ResponseUsuarioConsultarDTO.class))))
+    public ResponseEntity<?> listAllUsuarios() {
 
-        return usuarioService.listarUsuarios();
+        return usuarioService.listAllUsuarios();
     }
 
-    @GetMapping("cpfcnpj/{cpfcnpj}")
+    @GetMapping("/cpfcnpj/{cpfcnpj}")
     @Operation(summary = "Consultar Usuário pelo CPFCNPJ", description = "")
-    @ApiResponse(responseCode = "201", description = "Sucesso",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation =  UsuarioEntity.class)))
-    public ResponseEntity<?> pesqUsuarioCpfcnpj(@PathVariable String cpfcnpj) {
+    @ApiResponse(responseCode = "200", description = "Sucesso",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseUsuarioConsultarDTO.class)))
+    public ResponseEntity<?> SearchUsuarioCpfcnpj(@PathVariable String cpfcnpj) {
 
 
-        return usuarioService.pesqUsuarioCpfcnpj(cpfcnpj);
+        return usuarioService.SearchUsuarioCpfcnpj(cpfcnpj);
     }
 
-    @GetMapping("email/{email}")
+    @GetMapping("/email/{email}")
     @Operation(summary = "Consultar Usuário pelo Email ", description = "")
-    @ApiResponse(responseCode = "201", description = "Sucesso",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation =  UsuarioEntity.class)))
-    public ResponseEntity<?> listarUsuarios(@PathVariable String email) {
+    @ApiResponse(responseCode = "200", description = "Sucesso",
+            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseUsuarioConsultarDTO.class)))
+    public ResponseEntity<?> searchById(@PathVariable String email) {
 
-        return usuarioService.pesqUsuarioId(email);
+
+        return usuarioService.SearchUsuarioEmail(email);
     }
 
     @PostMapping("/{cpfcnpj}")
@@ -61,29 +63,20 @@ public class UsuarioController {
             Campo "situacao" Informar "A" = Ativo | "I" = Inativo | Default = "A" (Opcional) \n
             """)
     @ApiResponse(responseCode = "201", description = "Sucesso")
-    public ResponseEntity<?> criarUsuario(@PathVariable String cpfcnpj, @RequestBody RequestUsuarioCadastrarDTO usuarioRequest) {
+    public ResponseEntity<?> createUsuario(@PathVariable String cpfcnpj, @RequestBody RequestUsuarioCadastrarDTO usuarioRequest) {
 
-            UsuarioEntity usuarioEntity = new UsuarioEntity();
 
-            usuarioEntity.setEmail(usuarioRequest.getEmail());
-            usuarioEntity.setSenha(usuarioRequest.getSenha());
-            usuarioEntity.setSituacao(usuarioRequest.getSituacao());
-
-            if(cpfcnpj != null) {
-                usuarioEntity.getPessoaEntity().setCpfcnpj(cpfcnpj);
-            }
-        return usuarioService.criarUsuario(usuarioEntity);
+        return usuarioService.createUsuario(cpfcnpj, usuarioRequest);
     }
 
 
-    @PutMapping
+    @PutMapping("/{email}")
     @Operation(summary = "Atualizar Usuário", description = "")
-    @ApiResponse(responseCode = "201", description = "Sucesso",
-            content = @Content(mediaType = "application/json",
-                    schema = @Schema(implementation =  UsuarioEntity.class)))
-    public UsuarioEntity atualizarUsuario(@RequestBody UsuarioEntity usuarioEntity) {
+    @ApiResponse(responseCode = "200", description = "Sucesso")
+    public ResponseEntity<?> updateUsuario(@PathVariable String email, @RequestBody RequestUsuarioCadastrarDTO requestUsuarioCadastrarDTO) {
 
-        return usuarioService.atualizarUsuario(usuarioEntity);
+
+        return usuarioService.updateUsuario(email, requestUsuarioCadastrarDTO);
     }
 
 }
