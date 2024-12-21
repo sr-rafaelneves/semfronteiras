2fe3998512d6d87c0231cc16040648d5f8daef65
Gabriel Augusto <gabgustosilva@hotmail.com.br>
2024-12-20 22:09:34 -0300

    Update UsuarioRepository.java

--- src/main/java/br/com/rnconsulting/semfronteiras/repositories/UsuarioRepository.java
+++ src/main/java/br/com/rnconsulting/semfronteiras/repositories/UsuarioRepository.java
@@ -5,9 +5,13 @@ import br.com.rnconsulting.semfronteiras.entity.UsuarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface UsuarioRepository extends JpaRepository<UsuarioEntity, String> {
-    Optional<UsuarioEntity> findByPessoaEntityCpfcnpj(String cpfcnpj);
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, UUID> {
+    UsuarioEntity findByPessoaEntityCpfcnpj(String cpfcnpj);
+    UsuarioEntity findByEmail(String email);
+    List<UsuarioEntity> findAllByPessoaEntityCpfcnpj(String cpfcnpj);
 }
